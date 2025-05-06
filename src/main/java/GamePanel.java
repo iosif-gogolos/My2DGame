6844f14ff66f3f444e096cde52aaadd4217b9139
Iosif Gogolos <iosif.gogolos@gmail.com>
2025-05-06 10:49:45 +0200

    Inital commit, draw 2d field and measure time passed

---
+++ src/main/java/GamePanel.java
@@ -0,0 +1,79 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+    // Screen settings
+    final int originalTileSize = 16; //16x16 tile is the original size
+    final int scale= 3;
+
+    final int tileSize = originalTileSize * scale; //48 x 48 tile
+    final int maxScreenCol = 16; // change if you want
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    // FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;     // Start time to make character move 60 fps: keeps program running until stopping
+
+    int playerX = 100;    // Set Player's default position
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true); // for better rendering performance
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run(){
+        // Create game loop that will be the core of our game
+        while(gameThread != null){
+
+            long currentTime = System.nanoTime(); // Returns the current value of the running Java Virtual Machine's high-resolution time source in nanoseconds, //long currentTime2 =  System.currentTimeMillis(); // 1,000 milliseconds = 1 seconds
+            System.out.println("current Time: "+currentTime);
+
+            update();
+
+            repaint();
+            // --------------- We need to know how much time passed
+        }
+
+    }
+    public void update(){
+        // in our case playerSpeed is 4 pixels at start
+        if(keyH.upPressed == true){
+            playerY -= playerSpeed;
+        }else if(keyH.downPressed == true){
+            playerY += playerSpeed;
+        } else if(keyH.leftPressed == true){
+            playerX -= playerSpeed;
+        }else if(keyH.rightPressed == true){
+            playerX += playerSpeed;
+        }
+
+    }
+    public void paintComponent(Graphics g){
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
+
+    }
+}
